de0f3e8fd8120286b66c30e97b566d2abe04bb32
Abdelrahman mostafa <a.abdo.mae@gmail.com>
2023-09-29 21:20:44 +0300

    Client done

--- Client/src/Client.java
+++ Client/src/Client.java
@@ -7,62 +7,69 @@ import java.io.*;
 import java.net.Socket;
 
 public class Client {
-
     public static void main(String[] args) {
 
-        // Accessed from within inner class needs to be final or effectively final.
+        // First thing we need to do declare the file that we are going to send. 
         final File[] fileToSend = new File[1];
 
-        // Set the frame to house everything.
-        JFrame jFrame = new JFrame("WittCode's Client");
-        // Set the size of the frame.
-        jFrame.setSize(450, 450);
-        // Make the layout to be box layout that places its children on top of each other.
-        jFrame.setLayout(new BoxLayout(jFrame.getContentPane(), BoxLayout.Y_AXIS));
-        // Make it so when the frame is closed the program exits successfully.
+        JFrame jFrame = new JFrame("Client for sharing files.");
+        jFrame.setSize(350, 350);
+        
         jFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
-        // Title above panel.
-        JLabel jlTitle = new JLabel("WittCode's File Sender");
-        // Change the font family, size, and style.
-        jlTitle.setFont(new Font("Arial", Font.BOLD, 25));
-        // Add a border around the label for spacing.
+        JLabel jlTitle = new JLabel("Choose file(s) to share");
+        jlTitle.setFont(new Font("Arial", Font.BOLD, 26));
         jlTitle.setBorder(new EmptyBorder(20,0,10,0));
-        // Make it so the title is centered horizontally.
         jlTitle.setAlignmentX(Component.CENTER_ALIGNMENT);
 
-        // Label that has the file name.
         JLabel jlFileName = new JLabel("Choose a file to send.");
-        // Change the font.
         jlFileName.setFont(new Font("Arial", Font.BOLD, 20));
-        // Make a border for spacing.
+        jlFileName.setForeground(Color.DARK_GRAY);
         jlFileName.setBorder(new EmptyBorder(50, 0, 0, 0));
-        // Center the label on the x axis (horizontally).
         jlFileName.setAlignmentX(Component.CENTER_ALIGNMENT);
 
-        // Panel that contains the buttons.
         JPanel jpButton = new JPanel();
-        // Border for panel that houses buttons.
         jpButton.setBorder(new EmptyBorder(75, 0, 10, 0));
-        // Create send file button.
         JButton jbSendFile = new JButton("Send File");
-        // Set preferred size works for layout containers.
         jbSendFile.setPreferredSize(new Dimension(150, 75));
-        // Change the font style, type, and size for the button.
         jbSendFile.setFont(new Font("Arial", Font.BOLD, 20));
-        // Make the second button to choose a file.
-        JButton jbChooseFile = new JButton("Choose File");
-        // Set the size which must be preferred size for within a container.
-        jbChooseFile.setPreferredSize(new Dimension(150, 75));
-        // Set the font for the button.
-        jbChooseFile.setFont(new Font("Arial", Font.BOLD, 20));
+        jbSendFile.setBackground(new Color(70, 130, 180));
+        jbSendFile.setForeground(Color.WHITE);
+        jbSendFile.setFocusPainted(false);
+        jbSendFile.setBorder(BorderFactory.createRaisedBevelBorder());
 
-        // Add the buttons to the panel.
+        JButton jbChooseFile = new JButton("Choose File");
+        jbChooseFile.setPreferredSize(new Dimension(150, 75));
+        jbChooseFile.setFont(new Font("Arial", Font.BOLD, 20));
+        jbChooseFile.setBackground(new Color(70, 130, 180));  // SteelBlue color
+        jbChooseFile.setForeground(Color.WHITE);
+        jbChooseFile.setFocusPainted(false);
+        jbChooseFile.setBorder(BorderFactory.createRaisedBevelBorder());
+
+
         jpButton.add(jbSendFile);
         jpButton.add(jbChooseFile);
 
+        jbChooseFile.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseEntered(java.awt.event.MouseEvent evt) {
+                jbChooseFile.setBackground(new Color(135, 206, 250));  // LightSteelBlue color on hover
+            }
+
+            public void mouseExited(java.awt.event.MouseEvent evt) {
+                jbChooseFile.setBackground(new Color(70, 130, 180));  // Revert to original color on exit
+            }
+        });
+
+        jbSendFile.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseEntered(java.awt.event.MouseEvent evt) {
+                jbSendFile.setBackground(new Color(135, 206, 250));  // LightSteelBlue color on hover
+            }
+
+            public void mouseExited(java.awt.event.MouseEvent evt) {
+                jbSendFile.setBackground(new Color(70, 130, 180));  // Revert to original color on exit
+            }
+        });
+
         // Button action for choosing the file.
-        // This is an inner class so we need the fileToSend to be final.
         jbChooseFile.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
@@ -76,7 +83,11 @@ public class Client {
                     fileToSend[0] = jFileChooser.getSelectedFile();
                     // Change the text of the java swing label to have the file name.
                     jlFileName.setText("The file you want to send is: " + fileToSend[0].getName());
+                }else{
+                    // If no file is chosen then display this message.
+                    jlFileName.setText("No file chosen.");
                 }
+
             }
         });
 
@@ -84,6 +95,7 @@ public class Client {
         // Sends the file when the button is clicked.
         jbSendFile.addActionListener(new ActionListener() {
             @Override
+            @SuppressWarnings("resource")
             public void actionPerformed(ActionEvent e) {
                 // If a file has not yet been selected then display this message.
                 if (fileToSend[0] == null) {
@@ -91,19 +103,20 @@ public class Client {
                     // If a file has been selected then do the following.
                 } else {
                     try {
-                        // Create an input stream into the file you want to send.
+                        // Allowing us to into this file
                         FileInputStream fileInputStream = new FileInputStream(fileToSend[0].getAbsolutePath());
                         // Create a socket connection to connect with the server.
                         Socket socket = new Socket("localhost", 1234);
                         // Create an output stream to write to write to the server over the socket connection.
+                        // To basically send a file to our server.
                         DataOutputStream dataOutputStream = new DataOutputStream(socket.getOutputStream());
-                        // Get the name of the file you want to send and store it in filename.
+                        // Get the name of the file you want to send and store it.
                         String fileName = fileToSend[0].getName();
                         // Convert the name of the file into an array of bytes to be sent to the server.
                         byte[] fileNameBytes = fileName.getBytes();
                         // Create a byte array the size of the file so don't send too little or too much data to the server.
                         byte[] fileBytes = new byte[(int)fileToSend[0].length()];
-                        // Put the contents of the file into the array of bytes to be sent so these bytes can be sent to the server.
+                        // Put the contents of the file into the array of bytes to be sent so these bytes can be sent to the server. Read into fileBytes as it is empty.
                         fileInputStream.read(fileBytes);
                         // Send the length of the name of the file so server knows when to stop reading.
                         dataOutputStream.writeInt(fileNameBytes.length);
@@ -120,11 +133,15 @@ public class Client {
             }
         });
 
-        // Add everything to the frame and make it visible.
-        jFrame.add(jlTitle);
-        jFrame.add(jlFileName);
-        jFrame.add(jpButton);
-        jFrame.setVisible(true);
-    }
+        JPanel mainPanel = new JPanel();
+        mainPanel.setLayout(new BoxLayout(mainPanel, BoxLayout.Y_AXIS));
 
+        mainPanel.add(jlTitle);
+        mainPanel.add(jlFileName);
+        mainPanel.add(jpButton);
+
+        jFrame.add(mainPanel);
+        jFrame.setVisible(true);
+
+    }
 }
